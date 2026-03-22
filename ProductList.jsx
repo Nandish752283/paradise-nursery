d0022b2041f553ef5074da43d65bf53846b723fb
Nandish752283 <104014663+Nandish752283@users.noreply.github.com>
2026-03-22 15:52:21 +0100

    Create ProductList.jsx

---
+++ ProductList.jsx
@@ -0,0 +1,8 @@
+const plants = [
+  { id: 1, name: "Aloe Vera", price: 10, category: "Succulents" },
+  { id: 2, name: "Snake Plant", price: 15, category: "Indoor" },
+  { id: 3, name: "Fern", price: 12, category: "Outdoor" },
+  { id: 4, name: "Cactus", price: 8, category: "Succulents" },
+  { id: 5, name: "Bamboo", price: 20, category: "Indoor" },
+  { id: 6, name: "Palm", price: 18, category: "Outdoor" }
+];
