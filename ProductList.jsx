e1903061b122b4f544d104ab8c48eda2905a3a95
Nandish752283 <104014663+Nandish752283@users.noreply.github.com>
2026-03-22 16:04:53 +0100

    Update ProductList.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,8 +1,11 @@
 const plants = [
   { id: 1, name: "Aloe Vera", price: 10, category: "Succulents" },
-  { id: 2, name: "Snake Plant", price: 15, category: "Indoor" },
-  { id: 3, name: "Fern", price: 12, category: "Outdoor" },
-  { id: 4, name: "Cactus", price: 8, category: "Succulents" },
-  { id: 5, name: "Bamboo", price: 20, category: "Indoor" },
+  { id: 2, name: "Cactus", price: 8, category: "Succulents" },
+  { id: 3, name: "Snake Plant", price: 15, category: "Indoor" },
+  { id: 4, name: "Bamboo", price: 20, category: "Indoor" },
+  { id: 5, name: "Fern", price: 12, category: "Outdoor" },
   { id: 6, name: "Palm", price: 18, category: "Outdoor" }
 ];
+<button disabled={added} onClick={() => addToCart(plant)}>
+  Add to Cart
+</button>
